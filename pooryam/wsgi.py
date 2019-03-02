82f16f7c6c6d2c2d60ddcf09c60173a10cb3c652
AaronDoge <chendai2004@hotmail.com>
2019-03-02 17:25:37 +0800

    update

--- pooryam/wsgi.py
+++ pooryam/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "experimentProj.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pooryam.settings")
 
 application = get_wsgi_application()
