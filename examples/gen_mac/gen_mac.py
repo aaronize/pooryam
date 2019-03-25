bc484641cf7be588671b2bbc90741acaa3291140
aaron.chen <aaron.chen@ucloud.cn>
2019-03-25 21:45:20 +0800

    update

---
+++ examples/gen_mac/gen_mac.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import random
+
+
+def gen_mac():
+    mac1 = hex(random.randint(0, 255))[2:4]
+    if len(mac1) == 1:
+        mac1 = '0' + mac1
+    mac2 = hex(random.randint(0, 255))[2:4]
+    if len(mac2) == 1:
+        mac2 = '0' + mac2
+    mac3 = hex(random.randint(0, 255))[2:4]
+    if len(mac3) == 1:
+        mac3 = '0' + mac3
+    return mac1 + ':' + mac2 + ':' + mac3
+
+
+def gen_mac_2():
+    mac = ''
+    for i in range(0, 2, 1):
+        tmp = hex(random.randint(0, 255))[2:4]
+        if len(tmp) == 1:
+            tmp = '0' + tmp
+        if i == 2:
+            mac = mac + tmp
+        else:
+            mac = mac + tmp + ':'
+    return mac
+
+
+if __name__ == "__main__":
+    print 'generate mac: ', gen_mac()
+    print 'generate mac 2:', gen_mac_2()
