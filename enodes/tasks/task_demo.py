ae7443f4a17d169427ec7586ace348e01224e4bb
aaron.chen <aaron.chen@ucloud.cn>
2019-04-12 20:24:21 +0800

    update

--- enodes/tasks/task_demo.py
+++ enodes/tasks/task_demo.py
@@ -1,12 +1,14 @@
 from __future__ import absolute_import, unicode_literals
-from celery import Celery
-from enodes import config
+# from celery import Celery
+# from enodes import config
+from celery import shared_task
 import time
 
-app = Celery('tasks', broker=config.CELERY_BROKER_URL, backend=config.CELERY_BROKER_BACKEND)
+# app = Celery('tasks', broker=config.CELERY_BROKER_URL, backend=config.CELERY_BROKER_BACKEND)
 
 
-@app.task
+# @app.task
+@shared_task
 def send_mail(mail):
     print 'sending mail to {}...'.format(mail['to'])
     time.sleep(2.0)
