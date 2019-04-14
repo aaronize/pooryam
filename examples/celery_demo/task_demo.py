91e8f7c7b5bb982ba8e64f26e1556327dc8be64e
aaron.chen <aaron.chen@ucloud.cn>
2019-04-14 20:23:45 +0800

    update

--- examples/celery_demo/task_demo.py
+++ examples/celery_demo/task_demo.py
@@ -7,7 +7,13 @@ CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
 CELERY_BROKER_BACKEND = 'redis://127.0.0.1:6379/1'
 CELERY_BROKER_SERIALIZER = 'json'
 
-app = Celery(b'tasks', broker=CELERY_BROKER_URL, backend=CELERY_BROKER_BACKEND)
+app = Celery(b'celery_demo', broker=CELERY_BROKER_URL, backend=CELERY_BROKER_BACKEND)
+# app.broker_connection = ''
+# app.backend_cls = ''
+
+# app.conf.broker_url = ''
+# app.conf.result_backend = ''
+# app.conf.task_default_exchange = ''
 
 
 @app.task
@@ -19,4 +25,18 @@ def send_mail(mail):
 
 if __name__ == "__main__":
     ret = send_mail.delay(dict(to='aaron.chen@cdswit.cn'))
+    #
+    if ret.ready():
+        print 'result ready!'
+    else:
+        print 'result not ready! Pls wait for a while...'
+
+    try:
+        # timeout will throw exception
+        print 'get result:', ret.get(timeout=3)
+    except Exception, e:
+        print 'get result exception(s): ', str(e)
+
+    print 'traceback:', ret.traceback
+
     print 'task_id:', ret.task_id
